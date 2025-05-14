bb5dc24eba31ddbceda05abfcb3b36caeb1f2cdd
vie <vie.devr@gmail.com>
2025-05-14 10:24:54 +0300

    Init

---
+++ shl_arena.h
@@ -0,0 +1,55 @@
+void *aalloc(unsigned int amount);
+
+#ifdef SHL_ARENA_IMPLEMENTATION
+
+#include <stdlib.h>
+
+#include "log.h"
+
+#define DEFAULT_ARENA_CAP 256
+
+typedef struct Arena Arena;
+
+struct Arena {
+  char         *buffer;
+  unsigned int  cap, len;
+  Arena        *next;
+};
+
+Arena *arena = NULL;
+Arena *last_arena = NULL;
+
+Arena *create_arena(unsigned int cap) {
+  Arena *_arena = malloc(sizeof(Arena) + sizeof(char) * cap);
+  _arena->buffer = (char *) _arena + sizeof(Arena);
+  _arena->cap = cap;
+  _arena->len = 0;
+  _arena->next = NULL;
+  last_arena = _arena;
+  return _arena;
+}
+
+void *aalloc(unsigned int amount) {
+  if (amount > DEFAULT_ARENA_CAP)
+    arena = create_arena(amount);
+  else
+    arena = create_arena(DEFAULT_ARENA_CAP);
+
+  Arena *_arena = arena;
+
+  while (_arena->len + amount > _arena->cap) {
+    if (!_arena->next) {
+      unsigned int new_arena_cap = DEFAULT_ARENA_CAP;
+      if (amount > new_arena_cap)
+        new_arena_cap = amount;
+      _arena->next = create_arena(new_arena_cap);
+    }
+    _arena = _arena->next;
+  }
+
+  unsigned int len = _arena->len;
+  _arena->len += amount;
+  return _arena->buffer + len;
+}
+
+#endif // SHL_ARENA_IMPLEMENTATION
---
+++ shl_defs.h
@@ -0,0 +1,81 @@
+#ifndef DEFS_H
+#define DEFS_H
+
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define ARRAY_LEN(array) (sizeof(array) / sizeof(array[0]))
+
+#define Da(type)  \
+  struct {        \
+    type *items;  \
+    u32 len, cap; \
+  }
+
+#define DA_APPEND(da, element)                                        \
+  do {                                                                \
+    if ((da).cap <= (da).len) {                                       \
+      if ((da).cap != 0) {                                            \
+        while ((da).cap <= (da).len)                                  \
+          (da).cap *= 2;                                              \
+        (da).items = realloc((da).items, sizeof(element) * (da).cap); \
+      } else {                                                        \
+        (da).cap = 1;                                                 \
+        (da).items = malloc(sizeof(element));                         \
+      }                                                               \
+    }                                                                 \
+    (da).items[(da).len++] = element;                                 \
+  } while (0)
+
+#define DA_REMOVE(da) (da).items[--(da).len]
+
+#define DA_REMOVE_AT(da, index)                                 \
+  do {                                                          \
+    if ((index) < (da).len + 1) {                               \
+        memmove((da).items + (index), (da).items + (index) + 1, \
+                sizeof(*(da).items) * (da).len);                \
+      --(da).len;                                               \
+    }                                                           \
+  } while (0)
+
+#define LL_LEN(ll, type, target) \
+  do {                           \
+    *target = 0;                 \
+    type *next = ll;             \
+    while (next) {               \
+      ++*target;                 \
+      next = next->next;         \
+    }                            \
+  } while (0)
+
+#define LL_APPEND(ll, type)           \
+  do {                                \
+    type *new = aalloc(sizeof(type)); \
+    new->next = ll;                   \
+    ll = new;                         \
+  } while(0)
+
+#define LL_PREPEND(ll, ll_end, type)  \
+  do {                                \
+    type *new = aalloc(sizeof(type)); \
+    if (ll_end)                       \
+      ll_end->next = new;             \
+    else                              \
+      ll = new;                       \
+                                      \
+    ll_end = new;                     \
+  } while(0)
+
+typedef char           i8;
+typedef unsigned char  u8;
+typedef short          i16;
+typedef unsigned short u16;
+typedef int            i32;
+typedef unsigned int   u32;
+typedef long           i64;
+typedef unsigned long  u64;
+
+typedef float      f32;
+typedef long float f64;
+
+#endif // DEFS_H
